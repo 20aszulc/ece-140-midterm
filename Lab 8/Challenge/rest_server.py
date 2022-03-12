0f9188f6c0afa38d9ad878d6d063dd0565880ef4
aszulcucsd <33702922+20aszulc@users.noreply.github.com>
2022-03-12 06:34:25 -0800

    g

--- Lab 8/Challenge/rest_server.py
+++ Lab 8/Challenge/rest_server.py
@@ -1,6 +1,17 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+import json
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import FileResponse
+import mysql.connector as mysql
+from dotenv import load_dotenv
+import PID
+from datetime import datetime
+import os
+import cv2
+import numpy as np
 
 # This is used to render response through a JSON to the front-end
 from pyramid.renderers import render_to_response
@@ -16,52 +27,87 @@ load_dotenv('credentials.env') # Loads all details from the "credentials.env"
 db_host = os.environ['MYSQL_HOST']
 db_user = os.environ['MYSQL_USER']
 db_pass = os.environ['MYSQL_PASSWORD']
-db_name = os.environ['MYSQL_DATABASE']
+db_name = "Lab8"#os.environ['MYSQL_DATABASE']
 
-''' Instance Route to GET Student '''
-def get_student(req):
-  # Retrieve the route argument (this is not GET/POST data!)
-  the_id = req.matchdict['student_id']
 
-  # Connect to the database and retrieve the student
+def get_coordinates(name1, color_range, contour, size):
   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
   cursor = db.cursor()
-  cursor.execute("select * from students where id='%s';" % the_id)
+  #figure out address by importing and rundding pid controller
+  address1 = PID.run_pid(name1, color_range, contour, size)
+  #address1 = "figure me out"
+
+  #this puts the object and its address into found objects sql table
+  print(name1)
+  record = 1
+  value = 0
+  saved_name = name1
+      #if name already exists make it nameN where N is a number
+  while record is not None:
+    if value is not 0:
+      cursor.execute("Select * from found_objects where object_name= %s", (name1+str(value),))
+      record = cursor.fetchone()
+      saved_name =name1+str(value)
+    else:
+      cursor.execute("Select * from found_objects where object_name = %s", (name1,))
+      record = cursor.fetchone()
+    value = value + 1
+    print("value"+str(value))
+    db.commit()
+    print(record)
+  print("out of loop")
+  query = 'INSERT INTO found_objects(object_name, object_name_value, address) VALUES (%s, %s, %s)'
+  values = (saved_name, value, address1)
+  cursor.execute(query, values)
+  db.commit()
+
+  #this returns the coordinates of object to be shown on index
+  return address1
+
+''' Instance Route to GET object rank '''
+def object_rank(req):
+  # Retrieve the route argument (this is not GET/POST data!)
+  the_id = req.matchdict['object_rank_id']
+
+  # Connect to the database and retrieve the object
+  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+  cursor = db.cursor()
+  cursor.execute("select * from objects where id='%s';" % the_id)
   record = cursor.fetchone()
   db.close()
   if record is None:
     return ""
-
+  else:
+    coordinates = get_coordinates(str(record[1]), str(record[2]), str(record[3]), str(record[4]))
   # Format the result as key-value pairs
   response = {
 	'id':     	record[0],
 	'name':   	record[1],
-	'email':  	record[2],
-	'password':   record[3],
-	'age':    	record[4],
-	'datetime':   record[5].isoformat()
+	'color_range':  	record[2],
+	'contour':   record[3],
+	'size':    	record[4],
+    'coordinates': coordinates
   }
   return response
 
-''' Collection Route to GET Students '''
-def get_students(req):
+''' Collection Route to show the location of object list '''
+def store_location(req):
   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
   cursor = db.cursor()
-  cursor.execute("select id, name, email, password, age from Students;")
-  records = cursor.fetchall()
-  db.close()
 
-  data = {"students": records, "page_name": "Student Roster"}
-  return render_to_response('C:/Users/amber/PycharmProjects/ece140lab5/Tutorial2/index.html', data, request=req)
+  #retrieve info to post on website and in console
+  cursor.execute('SELECT * from found_objects ORDER BY id;')
+  my_result = cursor.fetchall()
+  records = "---SELECT---"
+  print('---This is the found_objects table\'s contents---<br>') #post found_objects info in console
+  [print(x) for x in my_result]
+  for x in my_result:
+    records = records + str(x) + "<br>"
+
+  return records
 
 def get_home(req):
-  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-  cursor = db.cursor()
-  cursor.execute("select id, name, email, password, age from Students;")
-  records = cursor.fetchall()
-  db.close()
-
-  return render_to_response('C:/Users/amber/PycharmProjects/ece140lab5/Tutorial2/index.html', {"students":records}, request=req)
+  return FileResponse('index.html')
 
 
 ''' Route Configurations '''
@@ -69,8 +115,8 @@ if __name__ == '__main__':
   with Configurator() as config:
 
  	# to use Jinja2 to render the template! 
-    config.include('pyramid_jinja2')
-    config.add_jinja2_renderer('.html')
+    #config.include('pyramid_jinja2')
+    #config.add_jinja2_renderer('.html')
 
 
     # Home route
@@ -78,11 +124,11 @@ if __name__ == '__main__':
     config.add_view(get_home, route_name='get_home')
 
 
-    config.add_route('get_students', '/students')
-    config.add_view(get_students, route_name='get_students', renderer='json')
+    config.add_route('store_location', '/store_location')
+    config.add_view(store_location, route_name='store_location', renderer='json')
 
-    config.add_route('get_student', '/student/{student_id}')
-    config.add_view(get_student, route_name='get_student', renderer='json')
+    config.add_route('object_rank', '/object_rank/{object_rank_id}')
+    config.add_view(object_rank, route_name='object_rank', renderer='json')
 
  	# For our static assets!
     config.add_static_view(name='/', path='./public', cache_max_age=3600)
